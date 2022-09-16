bf93b02e657c1d9e42ced31f3e2dc20cff2628e7
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-09-16 13:48:20 -0300

    Usuário vê detalhes do galpão e volta para a tela inicial

--- spec/system/user_views_homepage_spec.rb
+++ spec/system/user_views_homepage_spec.rb
@@ -5,7 +5,7 @@ describe 'Usuário visita a tela inicial' do
     # Arrange
 
     # Act
-    visit('/')
+    visit(root_path)
 
     # Assert
     expect(page).to have_content('Galpões & Estoque')
@@ -17,7 +17,7 @@ describe 'Usuário visita a tela inicial' do
     Warehouse.create(name: 'Maceió', code: 'MCZ',  city: 'Maceió', area: 50_000)
 
     # Act
-    visit('/')
+    visit(root_path)
 
     # Assert
     expect(page).not_to have_content('Não existem galpões cadastrados.')
@@ -37,7 +37,7 @@ describe 'Usuário visita a tela inicial' do
     # Arrange
     
     # Act
-    visit('/')
+    visit(root_path)
 
     # Assert
     expect(page).to have_content('Não existem galpões cadastrados.')
