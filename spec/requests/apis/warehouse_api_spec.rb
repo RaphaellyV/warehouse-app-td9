e6bd9f76de1712f9d5d18390b78484b5db246e3d
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-10-26 02:21:00 -0300

    Listando galpões via API

--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -19,6 +19,53 @@ describe 'Warehouse API' do
       
       expect(json_response["name"]).to eq 'Cuiabá'
       expect(json_response["code"]).to eq 'CGB'
+      expect(json_response.keys).not_to include("created_at")
+      expect(json_response.keys).not_to include("updated_at")
+    end
+
+    it 'fail if warehouse not found' do
+      # Arrange
+
+      # Act
+      get "/api/v1/warehouses/99999999999"
+
+      # Assert
+      expect(response.status).to eq 404
+    end
+  end
+
+  context 'GET /api/v1/warehouses' do
+    it 'lists all warehouses by name' do
+      # Arrange
+      Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, state: 'RJ',
+                        description: 'Galpão do Rio.', postal_code: '20000-000', address: 'Av. do Porto, 1000')
+      Warehouse.create!(name: 'Maceió', code: 'MCZ',  city: 'Maceió', area: 50_000, state: 'AL',
+                        description: 'Galpão de Maceió.', postal_code: '80000-000', address: 'Av. Atlântica, 50')
+
+      # Act
+      get '/api/v1/warehouses'
+
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include('application/json')
+      json_response = JSON.parse(response.body)
+      expect(json_response.class).to eq Array
+      expect(json_response.length).to eq 2
+      expect(json_response.first["name"]).to eq 'Maceió'
+      expect(json_response.second["name"]).to eq 'Rio'
+    end
+
+    it 'returns empty if there is no warehouse' do
+      # Arrange
+
+      # Act
+      get '/api/v1/warehouses'
+
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include('application/json')
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq []
     end
   end
 end
